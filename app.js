0dd3f01f20565ff071c72fe3e3b9452d71208c68
RaviTesla1 <ravibatalvi55@gmail.com>
2025-02-01 17:04:50 +0530

    Learning IIFE

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 // require('./xyz.js')
-import {calculateSum, x} from './sum.js'
+// const {x} = require('./calculate/sum.js')
+const  {calculateMultiply, calculateSum} = require('./calculate')
+const  data = require('./data.json')
 var name = "Namastey NodeJs"
 
 var a = 10;
 
 var b = 20 ;
 
-console.log(name)
+// console.log(name)
 
-console.log(a+b)
+// console.log(a+b)
 
 calculateSum(1,2);
-console.log('x---',x)
+calculateMultiply(1,2);
+console.log('data---',data)
